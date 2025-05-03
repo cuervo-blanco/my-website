6ad1f9aa2b1bb5126807c55da3976ea4716cc6c9
cuervo-blanco <jaios.228@gmail.com>
2025-05-03 12:41:55 -0400

    Added project titles to services section, and renamed to Work

--- src/components/sections/Services.js
+++ src/components/sections/Services.js
@@ -6,74 +6,63 @@ function Services() {
   return (
     <div id="services">
       <div id="services-header"></div>
-      <Title title="SERVICES" />
+      <Title title="WORK" />
       <h2>
         <div>My Service is Guaranteed Presence.</div>
       </h2>
       <div id="services-container">
-        {/* Production Sound */}
         <div id="service-1" className="services-block">
-          <h1>Production Sound</h1>
+          <h1>Audio Engineering</h1>
           <hr></hr>
           <p>
-            Got a film, interview, TV project, or something else brewing? Let's
-            get that sound just right. I'm here to give your visuals the
-            heartbeat they deserve. We can capture those perfect audio moments
-            on-site or mix things up post-production. And you know, every
-            project's got its own flavor, so I'm all ears to how you want to
-            work this out.
+                        <div>• <span className="project-title">See What I Wanna See</span> <span className="director-label">— Emilio Ramos</span></div>
+                        <div>• <span className="project-title">Inspired by True Events</span> <span className="director-label">— Knud Adams</span></div>
+                        <div>• <span className="project-title">Windows</span> <span className="director-label">— Mitchel Maxwell</span></div>
+                        <div>• <span className="project-title">SCAT! The Complex Lives of AL & DOT</span> <span className="director-label">— Jawole Willa Jo Zollar</span></div>
+                        <div>• <span className="project-title">The World According to Mickey Grant</span> <span className="director-label">— Nora Cole</span></div>
+                        <div>• <span className="project-title">Pay the Writer</span> <span className="director-label">— </span> Karen Carpenter</div>
+                        <div>• <span className="project-title">Little Match Girl</span> <span className="director-label">— Greg Ganakas</span></div>
+                        <div>• <span className="project-title">Cine Lechuza</span> <span className="director-label">— Aaron Sanchez</span></div>
+                        <div>• <span className="project-title">How to Disappear Completely</span> <span className="director-label">— Adam Hans</span></div>
+                        <div>• <span className="project-title">Queer Khalifa</span> <span className="director-label">— Danial Gondal</span></div>
+                        <div>• <span className="project-title">No Pienses en Monos</span> <span className="director-label">— Jaime Rivera</span></div>
+                        <div>• <span className="project-title">Dos Hombres Atentos</span> <span className="director-label">— Joserraul Ortiz</span></div>
+                        <div>• <span className="project-title">Si tienes que partir</span> <span className="director-label">— Mia Anderson</span></div>
+                        <div>• <span className="project-title">Los Monstruos No Existen</span> <span className="director-label">— Ariadna Pimentel</span></div>
+                        <div>• <span className="project-title">Carreras en el Aire</span> <span className="director-label">— David Paredes</span></div>
           </p>
-          <Button buttonText="Learn More" buttonLink="#contact" />
 
-          {/* Sound Design */}
         </div>
         <div id="service-2" className="services-block">
           <h1>Sound Design</h1>
           <hr></hr>
           <p>
-            Sound design is where the magic happens, and trust me, I live for
-            that magic. It's like cooking up the perfect dish with the right
-            ingredients. From the subtle sounds that make a scene to getting rid
-            of the noise we don't want – I've got it covered. Foley? ADR? Mixing
-            some beats? Let's make your audience feel every beat and whisper.{" "}
+                        <div>• <span className="project-title">Ain't Misbehaving</span> <span className="director-label">— Richard Maltby Jr.</span></div>
+                        <div>• <span className="project-title">About Time</span> <span className="director-label">— Richard Maltby Jr.</span></div>
+                        <div>• <span className="project-title">Matilda</span> <span className="director-label">— Thomas Netter</span></div>
+                        <div>• <span className="project-title">Juan Caballo</span> <span className="director-label">— Jose Carlos Jimenez</span></div>
+                        <div>• <span className="project-title">Negrita es la Noche Oscura de mi Alma</span> <span className="director-label">— Jaime Rivera</span></div>
+                        <div>• <span className="project-title">La Obra</span> <span className="director-label">— Jose Angel Perez</span></div>
+                        <div>• <span className="project-title">Hijas de la Invasión</span> <span className="director-label">— Eliana Arauz</span></div>
+                        <div>• <span className="project-title">Las Hortensias</span> <span className="director-label">— Daniel Delgado</span></div>
+                        <div>• <span className="project-title">El Pastor</span> <span className="director-label">— David Paredes</span></div>
+                        <div>• <span className="project-title">Huella Mínima</span> <span className="director-label">— Gerald Volta</span></div>
           </p>
-          <Button buttonText="Learn More" buttonLink="#contact" />
 
-          {/* General Sound Tech */}
         </div>
         <div id="service-3" className="services-block">
-          <h1>Live Sound Tech</h1>
+          <h1>Audio Programming</h1>
           <hr></hr>
           <p>
-            Live events? That's where things heat up! Theaters, concerts,
-            galleries – each has its own rhythm, and I'm here to tune in.
-            Setting up the sound, getting those levels on point, and ensuring
-            the vibe is just right is what I do. Whether it's a salsa beat or a
-            deep bass drop, let's team up and give your audience an audio
-            journey they won't forget.
+                        <div>• <span className="project-title"><a href="https://accuratesound.ca/hang-loose-convolver-hlc/">Hang Loose Convolver</a></span></div>
+                        <div>• <span className="project-title"><a href="https://github.com/cuervo-blanco/flanger_py">1D Flanger</a></span></div>
+                        <div>• <span className="project-title"><a href="https://github.com/cuervo-blanco/LPFilter">Low-Pass Filter Plugin</a></span></div>
+                        <div>• <span className="project-title"><a href="https://github.com/cuervo-blanco/ParametricEqualizer100">Parametric EQ Plugin</a></span></div>
+                        <div>• <span className="project-title"><a href="https://github.com/cuervo-blanco/GranularSynthesizer">Granular Synthesizer</a></span></div>
+                        <div>• <span className="project-title"><a href="https://github.com/cuervo-blanco/udp_voice">Real-Time Audio Communication</a></span></div>
+                        <div>• <span className="project-title"><a href="https://github.com/cuervo-blanco/convolver">File-to-File Convolver</a></span></div>
+                        <div>• <span className="project-title"><a href="https://github.com/cuervo-blanco">More Projects on GitHub</a></span></div>
           </p>
-          <Button buttonText="Learn More" buttonLink="#contact" />
-        </div>
-      </div>
-      <Title title="OTHER SERVICES" />
-      <div id="other-services">
-        <div id="other-service-1" className="other-services-block">
-          <h1>Audio Consultations</h1>
-          <hr></hr>
-          <p>
-            Require professional audio for your project, but don't know were to
-            begin? I'm here to assist.
-          </p>
-          <Button buttonText="Learn More" buttonLink="#contact" />
-        </div>
-        <div id="other-service-2" className="other-services-block">
-          <h1>Online Courses</h1>
-          <hr></hr>
-          <p>
-            Want to delve into Production Sound or Audio Programming? Schedule a
-            trial lesson today!
-          </p>
-          <Button buttonText="Learn More" buttonLink="#contact" />
         </div>
       </div>
     </div>
